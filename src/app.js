a88a0eda103d37aed92eccb571c8cf9d0a29e3b2
Osvaldo Castillo <casdev19@gmail.com>
2024-04-24 13:18:47 -0400

    24-04-24

--- src/app.js
+++ src/app.js
@@ -1,24 +1,21 @@
-import express from 'express'
-import employeesRoutes from './routes/employees.routes.js'
-import indexRoutes from './routes/index.routes.js'
-import morgan from 'morgan'
+import express from "express";
+import employeesRoutes from "./routes/employees.routes.js";
+import indexRoutes from "./routes/index.routes.js";
+import morgan from "morgan";
 
+const app = express();
 
-const app = express()
+app.use(morgan("dev"));
 
+app.use(express.json());
+app.use(indexRoutes);
+app.use("/api", employeesRoutes);
 
-app.use(morgan('dev'))
+//Para  otras rutas
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: "endpoint not found",
+  });
+});
 
-app.use(express.json())
-app.use(indexRoutes)
-app.use('/api',employeesRoutes)
-
-//Para  otras rutas 
-app.use((req,res,next)=>{
-    res.status(404).json({
-      "message": "endpoint not found"
-    })
-})
-
-
-export default app
+export default app;
